8b989dbf04732dc3d1e262985ded62b3c87219c7
Riyaz Ull Rabby <riyazullrabby999@gmail.com>
2024-09-25 00:48:40 +0600

    Update spin.sh

--- spin.sh
+++ spin.sh
@@ -11,7 +11,7 @@ spin() {
     spinner=( "█▒▒▒▒▒▒ lOADING ▒▒▒▒▒▒█" "▒█▒▒▒▒▒ LoADING ▒▒▒▒▒█▒" "▒▒█▒▒▒▒ LOaDING ▒▒▒▒█▒▒" "▒▒▒█▒▒▒ LOAdING ▒▒▒█▒▒▒" "▒▒▒▒█▒▒ LOADiNG ▒▒█▒▒▒▒" "▒▒▒▒▒█▒ LOADInG ▒█▒▒▒▒▒" "▒▒▒▒▒▒█ LOADINg █▒▒▒▒▒▒" "▒▒▒▒▒█▒ LOADInG ▒█▒▒▒▒▒" "▒▒▒▒█▒▒ LOADiNG ▒▒█▒▒▒▒" "▒▒▒█▒▒▒ LOAdING ▒▒▒█▒▒▒" "▒▒█▒▒▒▒ LOaDING ▒▒▒▒█▒▒" "▒█▒▒▒▒▒ LoADING ▒▒▒▒▒█▒" )
     while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
     for s in "${spinner[@]}";do
-    echo -en "\r${c}[ ${g}${s}${c} ]${n}\r"
+    echo -en "\r${c}[ ${g}${s}${c} ]${n}\r\033[KDone"
     sleep 0.20
     done
    	done
