a1fbf8f09f573806ead80d4a8962a5c5797c03f6
Riyaz Ull Rabby <riyazullrabby999@gmail.com>
2024-09-24 21:13:26 +0600

    Create spin.sh

---
+++ spin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+r="\033[1;31m"
+g="\033[1;32m"
+c="\033[1;36m"
+b="\033[1;34m"
+n="\033[0m"
+
+spin() {
+    pid=$!
+    spinner=( "█▒▒▒▒▒▒ lOADING ▒▒▒▒▒▒█" "▒█▒▒▒▒▒ LoADING ▒▒▒▒▒█▒" "▒▒█▒▒▒▒ LOaDING ▒▒▒▒█▒▒" "▒▒▒█▒▒▒ LOAdING ▒▒▒█▒▒▒" "▒▒▒▒█▒▒ LOADiNG ▒▒█▒▒▒▒" "▒▒▒▒▒█▒ LOADInG ▒█▒▒▒▒▒" "▒▒▒▒▒▒█ LOADINg █▒▒▒▒▒▒" "▒▒▒▒▒█▒ LOADInG ▒█▒▒▒▒▒" "▒▒▒▒█▒▒ LOADiNG ▒▒█▒▒▒▒" "▒▒▒█▒▒▒ LOAdING ▒▒▒█▒▒▒" "▒▒█▒▒▒▒ LOaDING ▒▒▒▒█▒▒" "▒█▒▒▒▒▒ LoADING ▒▒▒▒▒█▒" )
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    for s in "${spinner[@]}";do
+    tput civis
+    echo -ne "\r${c}[ ${g}${s}${c} ]\r"
+    sleep 0.20
+    done
+   	done
+   	tput cnorm
+   	echo -e "\n[ Done ]${n}"
+}
